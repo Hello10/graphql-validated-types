f8dae57715363de8ae11575f8efcd9f2e7c04b7c
Stephen Handley <stephen.handley@gmail.com>
2017-10-10 23:04:03 -0700

    forgot to add test

---
+++ src/GraphQLValidatedScalar.test.js
@@ -0,0 +1,41 @@
+const Assert = require('assert');
+
+const GraphQLValidatedScalar = require('./GraphQLValidatedScalar');
+
+describe('GraphQLValidatedScalar', ()=> {
+	describe('.validator(fn)', ()=> {
+		let VowelCountButNoLetterE;
+
+		beforeEach(()=> {
+			VowelCountButNoLetterE = new GraphQLValidatedScalar({
+				name: 'VowelCountButNoLetterE'
+			}).validator((value)=> {
+        if (value.match(/e/)) {
+          throw new Error('E is not allowed');
+        }
+        return value;
+      }).validator((value)=> {
+        let vowels = ['a', 'e', 'i', 'o', 'u'];
+        let count = 0;
+        for (let i = 0; i < value.length; i++) {
+          let letter = value[i];
+          if (vowels.indexOf(letter) !== -1) {
+            count++;
+          }
+        }
+        return count;
+      });
+		});
+
+		it('should validate and transfor input', ()=> {
+		    let count = VowelCountButNoLetterE.parseValue('animals');
+        Assert.equal(count, 3);
+		});
+
+		it('should throw when validator throws', ()=> {
+			Assert.throws(()=> {
+				VowelCountButNoLetterE.parseValue('forever');
+			}, /E is not allowed/);
+		});
+	});
+});
